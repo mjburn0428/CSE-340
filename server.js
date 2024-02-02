f5ec5ea63399aec8cd2021055ea79d12f169f44a
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-02-02 17:08:33 -0500

    Moved to MVC format for better rendering.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+
 
 
 /* ***********************
@@ -17,18 +19,15 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not at views root
+app.set("layout", "./layouts/layout") 
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
-// Index route
-app.get("/", function(req,res){
-  res.render("index",{title: "Home"})
-})
-
+// Index route 
+app.get("/", baseController.buildHome)
 
 
 /* ***********************
