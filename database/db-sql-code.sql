360f2439e71a7c1ee96e90491c4b1936bc054e96
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-02-06 18:15:18 -0500

    Updated for assignment 2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340_1;
+    OWNER TO cse340_4;
 
 -- Table structure for table classification
 CREATE TABLE public.classification (
