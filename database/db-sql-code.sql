2038ba40474baf0bdf34314e2c7a2ff49db76f6c
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-02-06 21:03:23 -0500

    Updated for assignment 2.

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -238,3 +238,8 @@ VALUES   (
     5
   );
 
+	--Update the inventory table to add "/vehicle in the middle of the file path in the inv_image and inv_thumbnail columns "
+UPDATE public.inventory
+SET
+	inv_image = REPLACE (inv_image, '/images/', '/images/vehicles'),
+	inv_thumbnail = REPLACE (inv_thumbnail, '/images/', 'images/vehicles/');
