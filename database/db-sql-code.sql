45c7ccf1d8704a265f545c1aeebe03c9f2baad5e
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-02-13 20:30:31 -0700

    Fixed for tetsing images fix

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340_7;
+    OWNER TO cse340_8;
 
 -- Table structure for table classification
 CREATE TABLE public.classification (
