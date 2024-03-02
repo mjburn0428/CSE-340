d5d92d0e6560b9e649b12fe7ffeb7c34884801fb
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-03-01 21:22:15 -0700

    Created login and register forms as well as finsihed Week 8 assignment.

---
+++ routes/accountRoute.js
@@ -0,0 +1,47 @@
+/* ***********************************
+ * Account Routes
+ * Unit 4, deliver login view activity
+ * ********************************* */
+
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
+
+/* ***********************************
+ * Deliver Login View
+ * Unit 4, deliver login view activity
+ * ********************************* */
+
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+/* *******************************************
+ * Deliver Registration View
+ * Unit 4, deliver login registration activity
+ * ***************************************** */
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister),);
+
+/* *************************************
+ * Process Registration
+ * Unit 4, process registration activity
+ * *********************************** */
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,6 +15,35 @@ const inventoryRoute = require("./routes/inventoryRoute");
 const utilities = require("./utilities");
 const session = require("express-session");
 const pool = require("./database")
+const bodyParser = require("body-parser")
+
+
+/* ******************
+ * Middleware
+ * *************** */
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  }),
+);
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 
@@ -33,8 +62,11 @@ app.use(static)
 // Index route 
 app.get("/", utilities.handleErrors(baseController.buildHome))
 
-// Inventory routes
+// Inventory route
 app.use("/inv", inventoryRoute)
+
+// Account route
+app.use("/account", require("./routes/accountRoute"))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
